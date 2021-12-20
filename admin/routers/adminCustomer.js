aeab14fcc89288800ca717c4eb86d990ae75dbf1
CNTT - Đỗ Vĩnh Hùng <hungdv@gmail.com>
2021-12-20 19:23:57 +0700

    update

--- admin/routers/adminCustomer.js
+++ admin/routers/adminCustomer.js
@@ -5,6 +5,7 @@ const parsePhoneNumber = require('libphonenumber-js')
 
 const Customer = require('../../customer/models/user')
 const Booking = require('../../customer/models/booking')
+const { CONSTANT_STATUS_BOOKING, CONSTANT_NOTIFICATION } = require('../../constant')
 
 const adminCustomer = express.Router()
 
@@ -81,5 +82,56 @@ adminCustomer.get('/admin/customer/recentBooking', authWithoutData, async (req, 
     }
 
 })
+adminCustomer.post('/admin/cancel/booking', authWithoutData, async (req, res) => {
+    try {
+        const { id } = req.body;
+        if (!id) {
+            res.status(400).send({ err: true, data: 'missing param' })
+        }
 
+        const update = await Booking.findOneAndUpdate({ _id: id }, { status: CONSTANT_STATUS_BOOKING.USER_CANCEL }, { new: true }).populate('cus_id', 'device_token');
+        pushNotificationTo_User(
+            [update?.cus_id?.device_token],
+            'Chuyến đi đã được huỷ',
+            'Chuyến đi đã được huỷ theo yêu cầu của bạn',
+            {
+                type: CONSTANT_NOTIFICATION.SYSTEM_CANCLE_BOOKING
+            })
+
+        res.status(200).send({ data: 'success', err: false })
+
+
+    } catch (error) {
+        console.log("error", error)
+        res.status(400).send({ err: true, error })
+
+    }
+
+})
+adminCustomer.post('/admin/finish/booking', authWithoutData, async (req, res) => {
+    try {
+        const { id } = req.body;
+        if (!id) {
+            res.status(400).send({ err: true, data: 'missing param' })
+        }
+
+        const update = await Booking.findOneAndUpdate({ _id: id }, { status: CONSTANT_STATUS_BOOKING.END }, { new: true }).populate('cus_id', 'device_token');
+        pushNotificationTo_User(
+            [update?.cus_id?.device_token],
+            'Chuyến đi đã kết thúc',
+            'Chuyến đi đã kết thúc theo yêu cầu của bạn',
+            {
+                type: CONSTANT_NOTIFICATION.DRIVER_DROP_OFF_CUSTOMER
+            })
+
+        res.status(200).send({ data: 'success', err: false })
+
+
+    } catch (error) {
+        console.log("error", error)
+        res.status(400).send({ err: true, error })
+
+    }
+
+})
 module.exports = adminCustomer;
